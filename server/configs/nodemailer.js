9fea5abd4709076c9c5d74c215c6a00b9611bb01
Fred Rider <rider48371@gmail.com>
2025-11-11 00:53:33 -0500

    Backend completed

---
+++ server/configs/nodemailer.js
@@ -0,0 +1,25 @@
+import nodemailer from 'nodemailer'
+
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.google.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: process.env.SENDER_EMAIL,
+        pass: process.env.WORKSPACE_APP_PASSWORD,
+    },
+});
+
+
+const sendEmail = async ({to, subject, body}) => {
+    const response = await transporter.sendMail({
+        from: process.env.SENDER_EMAIL,
+        to,
+        subject,
+        html: body,
+    });
+    return response
+};
+
+export default sendEmail
